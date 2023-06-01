43b56caa9da064780fd633db4a39faebfa6b79d7
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-01 05:55:06 +0530

    Initial

---
+++ FinalTest_FE/src/app/authentication/login-scout/login-scout.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-scout',
+  templateUrl: './login-scout.component.html',
+  styleUrls: ['./login-scout.component.scss']
+})
+export class LoginScoutComponent {
+
+
+toggleconfig114958={
+onBg: '#00b895',
+offBg: 'gray',
+knobBg: '#00b895',
+height: 10,
+width: 25,
+knobSize: 15,
+}
+
+}

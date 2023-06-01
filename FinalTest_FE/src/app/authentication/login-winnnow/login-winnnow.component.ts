9bf63251e3c7f773a98a59ef16070bdf42ac4456
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-01 05:55:09 +0530

    Initial

---
+++ FinalTest_FE/src/app/authentication/login-winnnow/login-winnnow.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-winnnow',
+  templateUrl: './login-winnnow.component.html',
+  styleUrls: ['./login-winnnow.component.scss']
+})
+export class LoginWinnnowComponent {
+
+
+toggleconfig1214={
+onBg: '#2c98f0',
+offBg: 'gray',
+knobBg: '#2c98f0',
+height: 8,
+width: 20,
+knobSize: 12,
+}
+
+}

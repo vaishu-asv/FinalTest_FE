26bf251100a0770927ca36751e84a1567212e163
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-01 05:55:04 +0530

    Initial

---
+++ FinalTest_FE/src/app/authentication/login-polaris/login-polaris.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-polaris',
+  templateUrl: './login-polaris.component.html',
+  styleUrls: ['./login-polaris.component.scss']
+})
+export class LoginPolarisComponent {
+
+}

d5fc80137064451d19a10bd134503e3fd96e2b68
vaishu-asv <134939088+vaishu-asv@users.noreply.github.com>
2023-06-01 05:55:12 +0530

    Initial

---
+++ FinalTest_FE/src/app/modules/advanced-search-result/advanced-search-result.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advanced-search-result',
+  templateUrl: './advanced-search-result.component.html',
+  styleUrls: ['./advanced-search-result.component.scss']
+})
+export class AdvancedSearchResultComponent {
+
+}
